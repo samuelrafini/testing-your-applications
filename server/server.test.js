c2578fadab677191c33d0407aff31cb16a17d44b
samuelrafini <Samuelrafini@gmail.com>
2018-03-03 10:34:56 +0100

    Organizing Test With describe()

--- server/server.test.js
+++ server/server.test.js
@@ -2,30 +2,48 @@ const request = require('supertest');
 const expect = require('expect');
 var app = require('./server').app;
 
-it('should return hello world responese', (done) =>{
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-           expect(res.body).toInclude({
-               error: 'Page not found.'
-           });
+
+
+//server
+//get
+//some test case 
+// 
+
+describe('Server', () => {
+
+    describe('GET /', () => {
+        it('should return hello world responese', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+
+    describe('Get /users', () => {
+        //make a new test
+        //assert 200
+        //assert that you exist in users array
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Sam',
+                        age: 25
+                    });
+                })
+                .end(done);
         })
-        .end(done);
+    })
+
 });
 
-//make a new test
-//assert 200
-//assert that you exist in users array
-it('should return my user object', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) =>{
-        expect(res.body).toInclude({
-            name: 'Sam',
-            age: 25
-        });
-    })
-    .end(done);
-})
+
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,39 +1,45 @@
 const expect =require('expect');
 const utils = require('./utils');
 
+describe('utils', () => {
 
-it('should add two numbers', () => {
-    var res = utils.add(33,11);
-
-    expect(res).toBe(44).toBeA('number');
-    // if (res !== 44){
-    // throw new Error(`Expected 44, but got ${res}`);
-    // }
-});
-
-it('should async add two numbers', (done) =>{
-    utils.asyncAdd(4,3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    describe('#add'), () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33,11);
+    
+         expect(res).toBe(44).toBeA('number');
+         // if (res !== 44){
+         // throw new Error(`Expected 44, but got ${res}`);
+         // }
+     });
+    }
+    
+    it('should async add two numbers', (done) =>{
+        utils.asyncAdd(4,3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
     });
-});
-
-
-it('should square a number', () =>{
-    var res = utils.square(4);
-
-    expect(res).toBe(16).toBeA('number');
-    // if(res !== 16){
-    // throw new Error(`Expected 16, but got ${res}`);
-    // }
-});
-
-it('should async square a number', (done) =>{
-    utils.asyncSquare(4,(square) => {
-        expect(square).toBe(16).toBeA('number');
-        done();
+    
+    
+    it('should square a number', () =>{
+        var res = utils.square(4);
+    
+        expect(res).toBe(16).toBeA('number');
+        // if(res !== 16){
+        // throw new Error(`Expected 16, but got ${res}`);
+        // }
     });
-});
+    
+    it('should async square a number', (done) =>{
+        utils.asyncSquare(4,(square) => {
+            expect(square).toBe(16).toBeA('number');
+            done();
+        });
+    });
+})
+
+
 
 it('should expect some values', () => {
     // expect(12).toNotBe(12);
